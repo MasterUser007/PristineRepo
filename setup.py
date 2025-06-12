fb8f091ad813b0fa302a9610588810d6bfd42028
MasterUser007 <your_email@domain.com>
2025-06-12 10:40:34 -0400

    Final metadata updates and repository cleanup

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='exosuite_optimized',
-    version='0.1.0',
-    packages=['exosuite'],
-    install_requires=[],
+    name="exosuite-optimized",
+    version="0.1.0",
+    description="Your project summary here",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/MasterUser007/PristineRepo",
+    packages=find_packages(),
 )
